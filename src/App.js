c4fd1d980e9a7abae357d2d3c4fd979c4607de80
Uche Azubuko <azubukouche@yahoo.com>
2021-11-18 06:45:34 +0000

    Building the Project UI - App header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__header">
+        <div className="app__headerWrapper">
+          <img
+            src=" https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt="Instagram original logo"
+          />
+          <button className="text__button">Logout</button>
+          <div className="app__headerButtons">
+            <button className="primary__button">Log in</button>
+            <button className="text__button">Sign up</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
